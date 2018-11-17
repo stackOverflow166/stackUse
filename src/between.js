cd61538cde7a2b0f66c79da7d1b2064fc28d7a32
wcypersonal <417177466@qq.com>
2018-11-17 21:47:40 +0800

    添加

--- src/between.js
+++ src/between.js
@@ -3,13 +3,37 @@ import { Field, reduxForm } from "redux-form";
 import Card from "@material-ui/core/Card";
 
 class RegisterShop extends Component {
+
     constructor(props){
         super(props)
         this.state ={
             first: 'first',
             second: 'second',
-            third: 'third'
+            third: 'third',
+            graphdata: [
+                [{y: 1, x: 10.7279501},{y: 2, x: 10.73239994}],
+                [{y: 3, x: 10.73684978}, {y: 4, x: 10.7413168}],
+                [{y: 5, x: 10.74576664},{y: 6, x: 10.7502327}]
+            ]
         }
+        this.renderGraphData = this.renderGraphData.bind(this)
+    }
+    static async getInitialProps(x){
+        await this.setState({
+            user: 'user'
+        })
+    }
+    componentDidMount(){
+        RegisterShop.getInitialProps()
+    }
+    renderGraphData(){
+        const graphArray = []
+        this.state.graphdata.map(items => (
+            items.map(item => (
+                graphArray.push(item)
+            ))
+        ))
+        console.log(graphArray)
     }
 
     renderField(field) {
@@ -32,6 +56,7 @@ class RegisterShop extends Component {
                 height: '200px'
             }
             styles = Object.assign(styles,firstStyle)
+            this.setState()
         }
         if (this.state.second === 'second'){
             const firstStyle = {
@@ -50,7 +75,7 @@ class RegisterShop extends Component {
     render() {
         const { handleSubmit } = this.props;
         return (
-            <div>
+            <div onClick={this.renderGraphData}>
                 <div style={this.setStyle()}>Test</div>
             </div>
         );
